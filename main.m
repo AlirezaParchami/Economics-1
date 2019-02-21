77a48672c969adf0ed9ec3d665e582e995cccb76
Alireza <alirezaprm76@gmail.com>
2019-02-21 08:04:51 +0330

    geomean and some theory change

--- main.m
+++ main.m
@@ -1,26 +1,18 @@
 % Read File
-t = readtable('D:\University Cources\Term 8\Projects\Economics-1\Exchange Rate\USD_to_EUR_June1_to_January1.xlsx');
+t = readtable('D:\University Cources\Term 8\Projects\Economics-1\Exchange Rate\EUR_to_Yen_June1_to_January1.xlsx');
 t.Properties.VariableNames = {'date' 'exchange'};
 t.exchange = str2double(t.exchange);
 digits(6)
 % Calculate Daily Volatility (Pt)
 Pt = [];
-abs_Pt = [];
 for i = 1:size(t,1)-1
-    tmp = vpa( (t.exchange(i+1) - t.exchange(i))/t.exchange(i) );
-    Pt = [Pt , tmp ];
-    abs_Pt = [abs_Pt , abs(tmp)];
+    Pt = [Pt , vpa( (t.exchange(i+1) - t.exchange(i))/t.exchange(i) ) ];
 end
 
-% what is Pt of last row?
-Pt = [Pt, vpa(0.001)];
-abs_Pt = [abs_Pt, vpa(0.001)];
-
+t( size(t,1) , : ) = [];  %Delete last row of t
 Pt = Pt';
-abs_Pt = abs_Pt';
-
 t.Pt = Pt;
-t.abs_Pt = abs_Pt;
+t.abs_Pt = abs(t.Pt);
 
 g = vpa(0.0001);
 L_array = [1 10 20 30 40 50 60 70 80 90 100];
@@ -35,12 +27,13 @@ for L = L_array
     q = size( q_h , 1 );
     entry = [entry, q];
     
-    P = geomean(t.abs_Pt);
-    P_h = geomean(q_h);
+    P = mean(t.abs_Pt);
+    P_h = mean(q_h);
+%%%% here
     if isequal( size(q_h,1) , 0)
         P_h = 0;
     end
-    P_l = geomean(q_l);
+    P_l = mean(q_l);
     if isequal( size(q_l,1) , 0)
         P_l = 0;
     end
@@ -49,14 +42,22 @@ for L = L_array
     entry = [entry, P, P_E, P_F];
 
     N = size(t, 1);
+%     L*g
+%     q_h
+%     [(1:size(q_l,1))' q_l]
+%     P_h
+%     P_l
+%     1 - L * P_l - L * g
+%     1 + P_h - L * g
+%     1 + L * P_l - L * g
+%     1 - L * P_l - L * g
+%     ((q - N) * log(1 - L * P_l - L * g) - q * log(1 + P_h - L * g))
+%     ( log(1 + L * P_l - L * g) - log(1 - L * P_l - L * g) )
     if isequal( (1 + P_h - L*g) , 0 )
-        disp("1")
         n0 = ((q - N)*log(1 - L * P_l - L * g)) / ( log(1 + L * P_l - L * g) - log(1 - L * P_l - L * g) );
     elseif isequal( (1 - L * P_l - L * g) , 0 )
-        disp("2")
         n0 = -q * log(1 + P_h - L * g) / ( log(1 + L * P_l - L * g) - log(1 - L * P_l - L * g) );
     else
-        disp("3..")
         n0 = ((q - N) * log(1 - L * P_l - L * g) - q * log(1 + P_h - L * g)) / ( log(1 + L * P_l - L * g) - log(1 - L * P_l - L * g) );
     end
 
@@ -67,10 +68,17 @@ for L = L_array
         P_loss_F = vpa( P_loss_F + (nchoosek(N-q , i) * power(0.5 , N-q)) );
     end
     P_loss = (P_E * P_loss_E) + (P_F * P_loss_F);
-    entry = [entry , n0 , P_loss_E , P_loss_F , P_loss];
+    
+    if isequal( size(q_h,1) , 0)
+        P_h = nan;
+    elseif (isequal( size(q_l, 1) , 0 ) )
+        P_l = nan;
+    end
+    
+    entry = [entry , P_h, P_l, n0 , P_loss_E , P_loss_F , P_loss];
     t2 = [t2;entry];
 disp('---------------------------------------------')
 end
-disp('t2(Leverage, g, q, P, P_E, P_F, n0, P_loss_E , P_loss_F , P_loss)')
+disp('t2(Leverage, g, q, P, P_E, P_F, P_h, P_l, n0, P_loss_E , P_loss_F , P_loss)')
 t2
 draw_plots(t, t2)
